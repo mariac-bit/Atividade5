e4de55cb35d4245a2875000f0e0229efda959785
Maria Cristina Silva de Queiroz <20211094010043@ifrn.local>
2024-07-23 10:04:21 -0300

    commit da cris

---
+++ objeto.py
@@ -0,0 +1,15 @@
+from pessoa import * 
+from usuario import * 
+
+user1=Pessoa()
+user1.cadastro()
+
+user2=Pessoa()
+user2.cadastro()
+
+user3=Pessoa()
+user3.cadastro()
+
+user3=Usuario()
+user3.getCodigo()
+user3.getDados()
